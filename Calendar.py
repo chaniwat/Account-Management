1ec38aaf277453788006b24bebfbd6ad2d277743
nexuszenith <nexuszenith@gmail.com>
2014-11-21 16:26:08 +0700

    Add docstring to Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,3 +1,5 @@
+"""Calendar Generator: Take input as the target year, Program will print\
+a calendar table from January to December of that target year"""
 import calendar
 year = input()
 for i in xrange(1, 13):
