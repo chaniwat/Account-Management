e89d225fde7d8612e409ea3330a989ba05b1bf26
nexuszenith <nexuszenith@gmail.com>
2014-12-16 15:56:44 +0700

    Nothing new

--- test2.py
+++ test2.py
@@ -1,13 +1,21 @@
+from Tkinter import *
+import tkMessageBox
 import Tkinter
-root = Tkinter.Tk(  )
-w = root.winfo_screenwidth()
-h = root.winfo_screenheight()
-rootsize = tuple(int(_) for _ in root.geometry().split('+')[0].split('x'))
-x = w/2 - rootsize[0]/2
-y = h/2 - rootsize[1]/2
-root.geometry("%dx%d+%d+%d" % (rootsize + (x, y)))
-for r in range(3):
-    for c in range(4):
-        Tkinter.Label(root, text='R%s/C%s'%(r,c),
-            borderwidth=1 ).grid(row=r,column=c)
-root.mainloop(  )
+
+top = Tk()
+
+mb=  Menubutton ( top, text="condiments", relief=RAISED )
+mb.grid()
+mb.menu  =  Menu ( mb, tearoff = 0 )
+mb["menu"]  =  mb.menu
+    
+mayoVar  = IntVar()
+ketchVar = IntVar()
+
+mb.menu.add_checkbutton ( label="mayo",
+                          variable=mayoVar )
+mb.menu.add_checkbutton ( label="ketchup",
+                          variable=ketchVar )
+
+mb.pack()
+top.mainloop()
